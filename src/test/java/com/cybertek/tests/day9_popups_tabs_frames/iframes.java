b2bba011c88e2255ee5e010f91af5b4caf21c4f1
AB1974 <bekemaylin@gmail.com>
2020-09-18 17:00:48 -0400

    first commmit

--- src/test/java/com/cybertek/tests/day9_popups_tabs_frames/iframes.java
+++ src/test/java/com/cybertek/tests/day9_popups_tabs_frames/iframes.java
@@ -4,6 +4,7 @@ import com.cybertek.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -88,6 +89,7 @@ public class iframes {
         //switch to bottom
         driver.switchTo().frame("frame-bottom");
         System.out.println(driver.findElement(By.tagName("body")).getText());
+
 
 
     }
